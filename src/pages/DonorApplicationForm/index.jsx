741f894423f461f7be635b776cd51bc00e807237
Dhanidayo <sarahjospeh@ymail.com>
2021-08-24 17:53:09 +0100

    created project folders

---
+++ src/pages/DonorApplicationForm/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function DonorApplication() {
+  return (
+      <div>
+          <h1>
+              This is the Donor Application page.
+          </h1>
+      </div>
+  );
+}
+
+export default DonorApplication;
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Signup() {
+  return (
+      <div>
+          <h1>
+              This is the signup page.
+          </h1>
+      </div>
+  );
+}
+
+export default Signup;
