744c012a4356a5c4bde939a93fb3bba907cd3075
Dhanidayo <sarahjospeh@ymail.com>
2021-09-24 15:54:01 +0100

    styled read more page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -37,7 +37,7 @@ function Contact() {
                                         <option value="--None">--None--</option>
                                         <option value="Get Started/About TechPow">Get Started/ About TechPow</option>
                                         <option value="Choosing a donee">Choosing a donee</option>
-                                        <option value="Donstion">Donation</option>
+                                        <option value="Donation">Donation</option>
                                         <option value="Email and Password Problem">Email and Password Problem</option>
                                         <option value="Report a bug and TroubleShooting">Report a bug and TroubleShooting</option>
                                         <option value="Report abuse">Report abuse</option>
@@ -46,9 +46,27 @@ function Contact() {
                                     </select>
                                 </div>
                             </div>
-                            <div className="text-area">
-                                <label for="help">How can we help you?</label>
-                                <textarea name="help" id="help" cols="60" rows="10" className="last-field"></textarea>
+                            <div className="form-box">
+                                <div>
+                                    <label for="help">How can we help you?</label>
+                                    <textarea name="help" id="help" cols="30" rows="10" className="contact-textarea"></textarea>    
+                                </div>
+                                <div>
+                                    <label for="topic">How are you feeling right now?</label>
+                                    <select name="feeling" id="feeling" className="form-field" required>
+                                        <option value="--None">--None--</option>
+                                        <option value="Happy">Happy</option>
+                                        <option value="Inspired">Inspired</option>
+                                        <option value="Excited">Excited</option>
+                                        <option value="Confused">Confused</option>
+                                        <option value="Angry">Angry</option>
+                                        <option value="Upset">Upset</option>
+                                        <option value="Worried">Worried</option>
+                                        <option value="Panic">Panic</option>
+                                    </select>
+                                </div>                   
+                            </div>
+                            <div>
                                 <button type="submit" className="btn-primary last-field help-btn">SEND MESSAGE</button>
                             </div>
                         </form>
