f3900b0d451072b1cbceee650522d00dc2e66298
Dhanidayo <sarahjospeh@ymail.com>
2021-10-01 02:14:36 +0100

    added chakra modals

--- src/components/Thankyou-cardDonee/index.jsx
+++ src/components/Thankyou-cardDonee/index.jsx
@@ -2,30 +2,46 @@ import { Link } from 'react-router-dom';
 import React from 'react';
 import ThankyouImage from '../../resources/images/Group.png';
 import './style.css';
-
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalBody,
+} from "@chakra-ui/react";
+//import DoneeApplicationForm from '../../pages/DoneeApplicationForm';
 
 function ThankyouCard() {
-    return (
-        <div className="thankyou-page">
-            <div className="thankyou-card">
-                <Link to="/LandingPage"><button className="close-btn">X</button></Link>
-    
-                <div className="thankyou-content">
-                    <img className="thankyouimage" src={ThankyouImage} alt="" />
 
-                    <div className="thankyou-right-flex">
-                        <div className="thankyou-div">
-                            <h2 className="center-text">Thanks</h2>
-                            <p className="blue-text thankyou-para">Our Team will review your application and will let you know about the result within approximately 20 days.
-                            </p>
+    return (
+        <>
+            <Modal>
+            <ModalOverlay />
+                <ModalContent>
+                    <ModalBody>
+                        <div className="thankyou-page">
+                            <div className="thankyou-card">
+                                <Link to="/LandingPage"><button className="close-btn">X</button></Link>
+                    
+                                <div className="thankyou-content">
+                                    <img className="thankyouimage" src={ThankyouImage} alt="" />
+
+                                    <div className="thankyou-right-flex">
+                                        <div className="thankyou-div">
+                                            <h2 className="center-text">Thanks</h2>
+                                            <p className="blue-text thankyou-para">Our Team will review your application and will let you know about the result within approximately 20 days.
+                                            </p>
+                                        </div>
+                                        <div className="proceed-btn">
+                                            <Link to="/LandingPage"><button className="btn btn-primary">Process to HomePage </button></Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="proceed-btn">
-                            <Link to="/LandingPage"><button className="btn btn-primary">Process to HomePage </button></Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+        </>
     );
 }
 export default ThankyouCard;
