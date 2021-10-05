c5b1d2c98f48447c3ffd6b6defb5e3d89bf40529
Dhanidayo <sarahjospeh@ymail.com>
2021-10-05 16:37:26 +0100

    fixed bugs

--- src/pages/DoneeApplicationForm/index.jsx
+++ src/pages/DoneeApplicationForm/index.jsx
@@ -5,21 +5,19 @@ import { AppContext } from '../../components/AppStateProvider';
 import { toast } from 'react-toastify';
 import DefaultLayout from '../../components/Layout/DefaultLayout';
 import TabNav from '../../components/TabNav';
-
-
 import './style.css';
 
 function DoneeApplicationForm() {
     const { state: { isLoggedIn } } = useContext(AppContext);
     const usehistory = useHistory();
     
-    // if (!isLoggedIn) {
-    //     usehistory.push('/Login');
-    //     return toast.error("Please Login to get started...",
-    //         {
-    //             position: toast.POSITION.TOP_CENTER
-    //         });
-    // }
+    if (!isLoggedIn) {
+        usehistory.push('/Login');
+        return toast.error("Please Login to get started...",
+            {
+                position: toast.POSITION.TOP_CENTER
+            });
+    }
 
     return (
         <DefaultLayout>
